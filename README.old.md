6b1a352147d8d0df655dbe355d7477ca0cbe8367
francis <jerylfranz460@gmail.com>
2023-03-25 00:19:47 +0400

    initialized stodo project

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# stodo
+a simple task app using react routers, firebases's user authentication and firestore sdks
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Router Switch here */}
+    </div>
+  );
+}
+
+export default App;
