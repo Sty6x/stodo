0def889d6eff92569459993bb81e111cf80e1d78
francis <jerylfranz460@gmail.com>
2023-03-29 17:27:01 +0400

    style: add structure to hero section

--- src/pages/index/Index.js
+++ src/pages/index/Index.js
@@ -13,7 +13,22 @@ export const Index = () => {
 					<NavLink to={"/auth/sign-up"}>Start for Free</NavLink>
 				</div>
 			</Navbar>
-			<main className={`${indexStyles.main}`}></main>
+			<main className={`${indexStyles.main}`}>
+				<section className={`${indexStyles.hero}`}>
+					<div className={` ${indexStyles.text} left`}>
+						<h1>
+							A Simple <span>Minimalistic</span> Task app.
+						</h1>
+						<p>
+							Organize all of your tasks,increase your productivity,
+							manage your time effectively, stay on top of your
+							priorities and get things done.
+						</p>
+						<Link to={"/auth/sign-up"}>Start for Free</Link>
+					</div>
+					<div className={`right ${indexStyles.image}`}></div>
+				</section>
+			</main>
 		</>
 	);
 };
