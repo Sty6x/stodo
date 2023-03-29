1ffeb0c4c278420d1581a0b484c6eb321baa3117
zaid-lp <jerylfranz460@gmail.com>
2023-03-29 10:48:31 +0400

    style: css reset

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,12 +1,16 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { Navbar } from "../components/navbar/Navbar";
 
 export const Index = () => {
 	return (
 		<>
 			<header>
-				<Navbar />
+				<Navbar>
+					<h1>
+						<Link to={"/"}>STODO</Link>
+					</h1>
+				</Navbar>
 			</header>
 			<main></main>
 		</>
