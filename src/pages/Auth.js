77f986976a14d0b475a97d34d6ed80b9f366405c
francis <jerylfranz460@gmail.com>
2023-03-30 16:24:31 +0400

    ref: removed unnecessary div

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 export const Auth = () => {
 	return (
-		<div>
+		<>
 			<Outlet />
-		</div>
+		</>
 	);
 };
--- src/pages/signInSignUp/signIn/SignIn.js
+++ src/pages/signInSignUp/signIn/SignIn.js
@@ -2,5 +2,5 @@ import React from "react";
 import signFormStyles from "../signform.module.scss";
 import signInStyles from "./signin.module.scss";
 export const SignIn = () => {
-	return <main className={signFormStyles.sign}></main>;
+	return <main className={signFormStyles.signForm}></main>;
 };
