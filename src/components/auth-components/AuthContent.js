822fced0bd9d9247da55fc67655b5bd76da290cf
francis <jerylfranz460@gmail.com>
2023-03-30 17:43:44 +0400

    style: add rudimentary styling to auth

--- src/components/auth-components/AuthContent.js
+++ src/components/auth-components/AuthContent.js
@@ -1,10 +1,15 @@
 import React from "react";
+import authContentStyles from "./authContent.module.scss";
 
 export const AuthContent = ({ content }) => {
 	return (
-		<div>
-			<div>{content.leftContent}</div>
-			<div>{content.rightContent}</div>
+		<div className={authContentStyles.authContentContainer}>
+			<div className={authContentStyles.leftContent}>
+				{content.leftContent}
+			</div>
+			<div className={authContentStyles.rightContent}>
+				{content.rightContent}
+			</div>
 		</div>
 	);
 };
