3325d3b87f7b917cb9ef58d3cd9214f340b855db
francis <jerylfranz460@gmail.com>
2023-03-31 06:07:59 +0400

    style: add style to btn

--- src/components/auth-components/AuthContent.js
+++ src/components/auth-components/AuthContent.js
@@ -1,6 +1,6 @@
 import React from "react";
 import authContentStyles from "./authContent.module.scss";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export const AuthContent = ({ content }) => {
 	const {
