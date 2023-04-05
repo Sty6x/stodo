31d750c23c8a9ba704e2c147ea16c7e6ebc4cc66
francis <jerylfranz460@gmail.com>
2023-04-05 10:26:10 +0400

    ref: add animation on eerror prompts mount and unmount

--- src/components/auth-components/AuthForm.js
+++ src/components/auth-components/AuthForm.js
@@ -1,15 +1,24 @@
 import React from "react";
 import authFormStyles from "./authFormStyles.module.scss";
+import { AnimatePresence, motion } from "framer-motion";
 export const AuthForm = ({ errorInput, onSubmit, children, buttonType }) => {
 	return (
-		<form className={authFormStyles.form} onSubmit={onSubmit}>
-			{errorInput.isError === true && (
-				<div className={authFormStyles.errorMessage}>
-					*{errorInput.message}
-				</div>
-			)}
+		<motion.div form className={authFormStyles.form} onSubmit={onSubmit}>
+			<AnimatePresence initial={false}>
+				{errorInput.isError === true && (
+					<motion.div
+						initial={{ opacity: 0.5, x: -15 }}
+						animate={{ opacity: 1, x: 0 }}
+						transition={{ duration: 0.7, type: "spring" }}
+						exit={{ opacity: 0, x: 15, transition: { duration: 0.2 } }}
+						className={authFormStyles.errorMessage}
+					>
+						<p>*{errorInput.message}</p>
+					</motion.div>
+				)}
+			</AnimatePresence>
 			{children}
 			<button type="submit">{buttonType}</button>
-		</form>
+		</motion.div>
 	);
 };
