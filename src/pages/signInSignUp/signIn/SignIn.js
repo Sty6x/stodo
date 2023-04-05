387068b9331f9bc20081519402b76a207f15bf1d
francis <jerylfranz460@gmail.com>
2023-04-05 15:33:03 +0400

    merged with validation branch

--- src/pages/signInSignUp/signIn/SignIn.js
+++ src/pages/signInSignUp/signIn/SignIn.js
@@ -1,25 +1,107 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { AuthContent } from "../../../components/auth-components/AuthContent";
 import { AuthForm } from "../../../components/auth-components/AuthForm";
-import signFormStyles from "../signform.module.scss";
+import signFormStyles from "../signInSignUp.module.scss";
 import signInStyles from "./signin.module.scss";
 
-const authContent = {
-	isSigningIn: true,
-	formComponent: (
-		<AuthForm
-			buttonType={"Sign in"}
-			onSubmit={(e) => {
-				e.preventDefault();
-			}}
-		/>
-	),
-	leftContentButton: { method: "Sign up", path: "/auth/sign-up" },
-};
-
 export const SignIn = () => {
+	const [userForm, setUserForm] = useState({
+		email: "",
+		password: "",
+	});
+	const [userCredentials, setUserCredentials] = useState(userForm); // use this state for submiting
+	const [inputError, setInputError] = useState({
+		isError: false,
+		message: null,
+	});
+	const authContent = {
+		isSigningIn: true,
+		formComponent: (
+			<AuthForm
+				errorInput={inputError}
+				buttonType={"Sign in"}
+				onSubmit={onSubmit}
+			>
+				<div>
+					<label htmlFor="email">Email</label>
+					<input
+						onChange={(e) => {
+							validateInput(e);
+							handleInputChange(e);
+						}}
+						value={userForm.email}
+						type={"email"}
+						required
+						id={"email"}
+					/>
+				</div>
+				<div>
+					<label htmlFor="pass">Password</label>
+					<input
+						onChange={(e) => {
+							validateInput(e);
+							handleInputChange(e);
+						}}
+						type={"password"}
+						value={userForm.password}
+						minLength={8}
+						required
+						id={"password"}
+					/>
+				</div>
+			</AuthForm>
+		),
+		leftContentButton: { method: "Sign up", path: "/auth/sign-up" },
+	};
+
+	function onSubmit(e) {
+		const form = e.target;
+		setUserCredentials({ ...userForm });
+		e.preventDefault();
+	}
+
+	function handleInputChange(e) {
+		const input = e.target;
+		setUserForm((prev) => ({ ...prev, [input.id]: input.value }));
+	}
+
+	function validateInput(e) {
+		const input = e.target;
+		console.log(input);
+		console.log(input.validity.valid);
+		if (!input.validity.valid) {
+			setInputError((prev) => ({
+				isError: true,
+				message: showError(input),
+			}));
+		} else {
+			setInputError({ isError: false, message: null });
+		}
+	}
+
+	function showError(input) {
+		const errors = {
+			missingValue: "Please Enter Your Email and Password",
+			email: "You Must Enter an Email",
+			password: "A User's Password should not be less than 8 characters",
+		};
+		if (input.validity.valueMissing) {
+			return errors.missingValue;
+		}
+		if (input.validity.typeMismatch) {
+			return errors.email;
+		}
+		if (input.validity.tooShort) {
+			return errors.password;
+		}
+	}
+
+	useEffect(() => {
+		console.log(inputError);
+	}, [inputError]);
+
 	return (
-		<main className={signFormStyles.signForm}>
+		<main className={signFormStyles.signPage}>
 			<AuthContent content={authContent} />
 		</main>
 	);
--- src/pages/signInSignUp/signup/SignUp.js
+++ src/pages/signInSignUp/signup/SignUp.js
@@ -1,30 +1,151 @@
-import React from "react";
-import signFormStyles from "../signform.module.scss";
+import React, { useEffect, useState } from "react";
+import signFormStyles from "../signInSignUp.module.scss";
 import signUpStyles from "./signup.module.scss";
 import { AuthForm } from "../../../components/auth-components/AuthForm";
 import { AuthContent } from "../../../components/auth-components/AuthContent";
 
-const authContent = {
-	isSigningIn: false,
-	formComponent: (
-		<AuthForm
-			buttonType={"Sign Up"}
-			onSubmit={(e) => {
-				e.preventDefault();
-			}}
-		>
-			<div className={signFormStyles.input}>
-				<label htmlFor="conf-pass">Confirm Password</label>
-				<input type={"password"} id={"conf-pass"} />
-			</div>
-		</AuthForm>
-	),
-	leftContentButton: { method: "Sign in", path: "/auth/sign-in" },
-};
-
 export const SignUp = () => {
+	const [inputError, setInputError] = useState({
+		isError: false,
+		message: null,
+	});
+	const [userForm, setUserForm] = useState({
+		email: "",
+		password: "",
+		passwordConfirmation: "",
+	});
+	const [passwordConfirmed, setPasswordConfirmed] = useState(false);
+
+	const [userCredentials, setUserCredentials] = useState(userForm); // use this state for submiting
+
+	const authContent = {
+		isSigningIn: false,
+		formComponent: (
+			<AuthForm
+				errorInput={inputError}
+				buttonType={"Sign Up"}
+				onSubmit={onSubmit}
+			>
+				<div>
+					<label htmlFor="email">Email</label>
+					<input
+						onChange={(e) => {
+							validateInput(e);
+							handleInputChange(e);
+						}}
+						type={"email"}
+						required
+						id={"email"}
+					/>
+				</div>
+				<div>
+					<label htmlFor="pass">Password</label>
+					<input
+						onChange={(e) => {
+							validateInput(e);
+							handleInputChange(e);
+						}}
+						type={"password"}
+						minLength={8}
+						required
+						id={"password"}
+					/>
+				</div>
+				<div>
+					<label htmlFor="conf-pass">Confirm Password</label>
+					<input
+						onChange={(e) => {
+							validateInput(e);
+							handleInputChange(e);
+						}}
+						type={"password"}
+						required
+						id={"passwordConfirmation"}
+					/>
+				</div>
+			</AuthForm>
+		),
+		leftContentButton: { method: "Sign in", path: "/auth/sign-in" },
+	};
+
+	function handleInputChange(e) {
+		const input = e.target;
+		setUserForm((prev) => ({ ...prev, [input.id]: input.value }));
+	}
+
+	function onSubmit(e) {
+		e.preventDefault();
+		setUserCredentials(userForm);
+	}
+
+	function checkPasswordConfirmation() {
+		const passArr = userForm.password.split("");
+		const passConfArr = userForm.passwordConfirmation.split("");
+		if (
+			passArr.length > passConfArr.length ||
+			passConfArr.length > passArr.length
+		) {
+			console.log("password does not match");
+			return false;
+		}
+		for (let i = 0; i < passConfArr.length; i++) {
+			if (passArr[i] === passConfArr[i]) {
+				console.log({ pass: passArr[i], conf: passConfArr[i] });
+			} else {
+				console.log("password does not match");
+				return false;
+			}
+		}
+		// return true if password checking is complete === true
+		// would automatically return false if any of the elements doesnt match
+		return true;
+	}
+
+	function validateInput(e) {
+		const input = e.target;
+		console.log(input);
+		console.log(passwordConfirmed);
+		if (!input.validity.valid || passwordConfirmed === false) {
+			setInputError((prev) => ({
+				isError: true,
+				message: showError(input),
+			}));
+		} else {
+			setInputError({ isError: false, message: null });
+		}
+	}
+
+	function showError(input) {
+		const errors = {
+			missingValue: "Please Enter Your Email and Password",
+			email: "You Must Enter a Valid Email",
+			password: "A User's Password should not be less than 8 characters",
+			passConf: "Your Password does not match",
+		};
+		if (input.validity.valueMissing) {
+			return errors.missingValue;
+		}
+		if (input.validity.typeMismatch) {
+			return errors.email;
+		}
+		if (input.validity.tooShort) {
+			return errors.password;
+		}
+		if (passwordConfirmed === false) {
+			return errors.passConf;
+		}
+	}
+
+	useEffect(() => {
+		if (checkPasswordConfirmation()) {
+			setPasswordConfirmed((prev) => true);
+		} else {
+			setPasswordConfirmed((prev) => false);
+		}
+	}, [userForm.password, userForm.passwordConfirmation]);
+
 	return (
-		<main className={signFormStyles.signForm}>
+		<main className={signFormStyles.signPage}>
 			<AuthContent content={authContent} />
 		</main>
 	);
