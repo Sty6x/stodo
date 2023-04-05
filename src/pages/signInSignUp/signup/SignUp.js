4211f7eacbd6e8e9f52d76d4dbc84ca3c72349ba
francis <jerylfranz460@gmail.com>
2023-04-05 10:28:15 +0400

    fix: missing dependencies

--- src/pages/signInSignUp/signup/SignUp.js
+++ src/pages/signInSignUp/signup/SignUp.js
@@ -3,46 +3,53 @@ import signFormStyles from "../signInSignUp.module.scss";
 import signUpStyles from "./signup.module.scss";
 import { AuthForm } from "../../../components/auth-components/AuthForm";
 import { AuthContent } from "../../../components/auth-components/AuthContent";
-
-const authContent = {
-	isSigningIn: false,
-	formComponent: (
-		<AuthForm
-			buttonType={"Sign Up"}
-			onSubmit={(e) => {
-				e.preventDefault();
-			}}
-		>
-			<div>
-				<label htmlFor="email">Email</label>
-				<input type={"email"} required id={"email"} />
-			</div>
-			<div>
-				<label htmlFor="pass">Password</label>
-				<input
-					type={"password"}
-					minLength={8}
-					maxLength={16}
-					required
-					id={"pass"}
-				/>
-			</div>
-			<div>
-				<label htmlFor="conf-pass">Confirm Password</label>
-				<input
-					type={"password"}
-					minLength={8}
-					maxLength={16}
-					required
-					id={"conf-pass"}
-				/>
-			</div>
-		</AuthForm>
-	),
-	leftContentButton: { method: "Sign in", path: "/auth/sign-in" },
-};
+import { useState } from "react";
 
 export const SignUp = () => {
+	const [inputError, setInputError] = useState({
+		isError: false,
+		message: null,
+	});
+
+	const authContent = {
+		isSigningIn: false,
+		formComponent: (
+			<AuthForm
+				errorInput={inputError}
+				buttonType={"Sign Up"}
+				onSubmit={(e) => {
+					e.preventDefault();
+				}}
+			>
+				<div>
+					<label htmlFor="email">Email</label>
+					<input type={"email"} required id={"email"} />
+				</div>
+				<div>
+					<label htmlFor="pass">Password</label>
+					<input
+						type={"password"}
+						minLength={8}
+						maxLength={16}
+						required
+						id={"pass"}
+					/>
+				</div>
+				<div>
+					<label htmlFor="conf-pass">Confirm Password</label>
+					<input
+						type={"password"}
+						minLength={8}
+						maxLength={16}
+						required
+						id={"conf-pass"}
+					/>
+				</div>
+			</AuthForm>
+		),
+		leftContentButton: { method: "Sign in", path: "/auth/sign-in" },
+	};
+
 	return (
 		<main className={signFormStyles.signPage}>
 			<AuthContent content={authContent} />
