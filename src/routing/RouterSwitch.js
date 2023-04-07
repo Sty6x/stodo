75146cff16d89c268edc14360bdf1b79997e0e4e
francis <jerylfranz460@gmail.com>
2023-04-07 23:39:09 +0400

    ref: redirecting users after signin in to /app/today for now for testing

--- src/routing/RouterSwitch.js
+++ src/routing/RouterSwitch.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink, Route, Routes } from "react-router-dom";
+import { FirebaseContext } from "../App";
+import { onAuthStateChanged } from "firebase/auth";
 
 export const RouterSwitch = ({ importRoutes }) => {
+	const { navigate, auth } = useContext(FirebaseContext);
+
+	onAuthStateChanged(auth, (user) => {
+		if (user) {
+			navigate("/app/today");
+		} else {
+			console.log("user is not signed in");
+		}
+	});
+
 	const renderRoute = importRoutes.map((route) => {
 		const sub =
 			route.subRoutes &&
