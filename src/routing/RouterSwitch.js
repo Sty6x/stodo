8d068d36f3e6f407392ce5682da7f99cf0f77f23
francis <jerylfranz460@gmail.com>
2023-04-08 00:08:13 +0400

    mumbo jumbo

--- src/routing/RouterSwitch.js
+++ src/routing/RouterSwitch.js
@@ -2,20 +2,35 @@ import React, { useContext, useEffect } from "react";
 import { NavLink, Route, Routes } from "react-router-dom";
 import { FirebaseContext } from "../App";
 import { onAuthStateChanged } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
 
 export const RouterSwitch = ({ importRoutes }) => {
-	const { navigate, auth } = useContext(FirebaseContext);
+	const { navigate, auth, db } = useContext(FirebaseContext);
 
 	useEffect(() => {
-		const checkUsersMethod = onAuthStateChanged(auth, (user) => {
+		const redirectIfNotLoggedIn = onAuthStateChanged(auth, (user) => {
 			if (user) {
-				navigate("/app/today");
+				checkIfUserAlreadyExists(user);
 			} else {
 				console.log("user is not signed in");
 			}
 		});
-		return checkUsersMethod;
+		return redirectIfNotLoggedIn;
 	}, []);
+
+	async function checkIfUserAlreadyExists(user) {
+		try {
+			const userDocref = doc(db, `/user/${user.uid}`);
+			const userDoc = await getDoc(user);
+			if (userDoc.exists()) {
+				return navigate("/app/today");
+			}
+			return navigate("/auth/sign-up/create-profile");
+		} catch (err) {
+			console.log("unable to fetch user path");
+			throw err;
+		}
+	}
 
 	const renderRoute = importRoutes.map((route) => {
 		const sub =
