345b2838154019a7429a225e6a9bfd4a0f4e640f
francis <jerylfranz460@gmail.com>
2023-04-07 17:29:19 +0400

    ref: add context provider

--- src/App.js
+++ src/App.js
@@ -2,13 +2,39 @@ import { Routes } from "react-router-dom";
 import "./App.css";
 import { RouterSwitch } from "./routing/RouterSwitch";
 import { ROUTES } from "./routing/routes";
+import { React, createContext } from "react";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDQD7TWt0E-m5zsMFTLfxbN-_uwUhw4saE",
+  authDomain: "stodo-f3ce7.firebaseapp.com",
+  projectId: "stodo-f3ce7",
+  storageBucket: "stodo-f3ce7.appspot.com",
+  messagingSenderId: "270403343540",
+  appId: "1:270403343540:web:ee854a268fe1fda14d73e1",
+  measurementId: "G-YS12MGWVZ3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const FirebaseContext = createContext()
 
 function App() {
-	return (
-		<div className="App">
-			<RouterSwitch importRoutes={ROUTES} />
-		</div>
-	);
+  return (
+    <div className="App">
+      <FirebaseContext.Provider>
+        <RouterSwitch importRoutes={ROUTES} />
+      </FirebaseContext.Provider>
+    </div>
+  );
 }
 
 export default App;
