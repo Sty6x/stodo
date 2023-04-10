a02b6bb155d36af330ce20d08cd6e7afd6a1e329
francis <jerylfranz460@gmail.com>
2023-04-10 21:49:42 +0400

    ref: add animation on component mount

--- src/pages/auth-page/createProfile/CreateProfile.js
+++ src/pages/auth-page/createProfile/CreateProfile.js
@@ -1,25 +1,32 @@
 import React from "react";
 import authpagesStyle from "../authpages.module.scss";
 import profileCreateStyles from "./createProfile.module.scss";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 export const CreateProfile = () => {
 	return (
 		<main className={`${authpagesStyle.page} ${profileCreateStyles.page}`}>
-			<section className={profileCreateStyles.content}>
-				<h1>What Should We Call You?</h1>
-				<div className={profileCreateStyles.inputContainer}>
-					<label htmlFor="name">Enter Your Name:</label>
-					<div className={profileCreateStyles.innerInputContainer}>
-						<input name="name" id="name" />
+			<AnimatePresence>
+				<motion.section
+					initial={{ opacity: 0, y: -100 }}
+					animate={{ opacity: 1, y: 0 }}
+					exit={{ y: 100, opacity: 0 }}
+					className={profileCreateStyles.content}
+				>
+					<motion.h1>What Should We Call You?</motion.h1>
+					<div className={profileCreateStyles.inputContainer}>
+						<label htmlFor="name">Enter Your Name:</label>
+						<div className={profileCreateStyles.innerInputContainer}>
+							<input name="name" id="name" />
 
-						<motion.button
-							whileHover={{ x: [null, 15, 10] }}
-							transition={{ duration: 0.3, type: "spring" }}
-						></motion.button>
+							<motion.button
+								whileHover={{ x: [null, 15, 10] }}
+								transition={{ duration: 0.3, type: "spring" }}
+							></motion.button>
+						</div>
 					</div>
-				</div>
-			</section>
+				</motion.section>
+			</AnimatePresence>
 		</main>
 	);
 };
