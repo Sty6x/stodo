a1cdc7aa85253c6e5323ef20be0d8cc8c5db20ea
francis <jerylfranz460@gmail.com>
2023-04-11 15:16:11 +0400

    ref: yes

--- src/components/auth-components/AuthContent.js
+++ src/components/auth-components/AuthContent.js
@@ -12,56 +12,52 @@ export const AuthContent = ({ content, onSuccess }) => {
 		console.log({ success: onSuccess });
 	}, [onSuccess]);
 	return (
-		<AnimatePresence mode="wait">
-			<motion.div
-				initial={{ opacity: 0, y: -200 }}
-				animate={{ opacity: 1, y: [null, 0] }}
-				exit={{ opacity: 0, y: 200 }}
-				transition={{ duration: 0.6, type: "spring", damping: 10 }}
-				className={authContentStyles.authContentContainer}
-			>
-				<div className={authContentStyles.leftContent}>
-					<motion.button
-						whileHover={{
-							x: [null, 10, -15, 0],
-							transition: { duration: 0.6 },
-						}}
-						onClick={() => {
-							navigate("/");
-						}}
-						className={authContentStyles.navigationArrow}
-					></motion.button>
-					<div className={authContentStyles.textContentContainer}>
-						<h1>
-							{isSigningIn ? "New Here?" : "Already Have an Account?"}
-						</h1>
+		<motion.div
+			key={"authContent"}
+			initial={{ opacity: 0, y: -200 }}
+			animate={{ opacity: 1, y: [null, 0] }}
+			transition={{ duration: 0.6, type: "spring", damping: 10 }}
+			className={authContentStyles.authContentContainer}
+		>
+			<div className={authContentStyles.leftContent}>
+				<motion.button
+					whileHover={{
+						x: [null, 10, -15, 0],
+						transition: { duration: 0.6 },
+					}}
+					onClick={() => {
+						navigate("/");
+					}}
+					className={authContentStyles.navigationArrow}
+				></motion.button>
+				<div className={authContentStyles.textContentContainer}>
+					<h1>{isSigningIn ? "New Here?" : "Already Have an Account?"}</h1>
+					<p>
+						{isSigningIn
+							? "Sign up Here to Start Organizing Your Thoughts"
+							: "Login Here to Start Organizing Your Thoughts"}
+					</p>
+					<motion.div>
+						<Link to={leftContentButton.path}>
+							{leftContentButton.method}
+						</Link>
+					</motion.div>
+				</div>
+			</div>
+			<div className={authContentStyles.rightContent}>
+				<div className={authContentStyles.formContentContainer}>
+					<div className={authContentStyles.greetContainer}>
+						<img src={logo} />
+						<h1>{isSigningIn ? "Welcome Back!" : "Welcome!"}</h1>
 						<p>
 							{isSigningIn
-								? "Sign up Here to Start Organizing Your Thoughts"
-								: "Login Here to Start Organizing Your Thoughts"}
+								? "Login to Your Account"
+								: "Create Your Account"}
 						</p>
-						<motion.div>
-							<Link to={leftContentButton.path}>
-								{leftContentButton.method}
-							</Link>
-						</motion.div>
 					</div>
+					{formComponent}
 				</div>
-				<div className={authContentStyles.rightContent}>
-					<div className={authContentStyles.formContentContainer}>
-						<div className={authContentStyles.greetContainer}>
-							<img src={logo} />
-							<h1>{isSigningIn ? "Welcome Back!" : "Welcome!"}</h1>
-							<p>
-								{isSigningIn
-									? "Login to Your Account"
-									: "Create Your Account"}
-							</p>
-						</div>
-						{formComponent}
-					</div>
-				</div>
-			</motion.div>
-		</AnimatePresence>
+			</div>
+		</motion.div>
 	);
 };
