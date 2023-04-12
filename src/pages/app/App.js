c326a7e5b912e07d776dcb9136955ef516e9d5ac
francis <jerylfranz460@gmail.com>
2023-04-12 22:10:35 +0400

    ref: add temporary signout button

--- src/pages/app/App.js
+++ src/pages/app/App.js
@@ -1,13 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import { Logo } from "../../components/logo/Logo";
 import { Navbar } from "../../components/navbar/Navbar";
 import appStyles from "./app.module.scss";
+import { signOut } from "firebase/auth";
+import { FirebaseContext } from "../../App";
+
 
 export const App = () => {
+  const {auth} = useContext(FirebaseContext);
   return (
     <>
       <Navbar>
+			<button
+				onClick={async (e) => {
+          signOut(auth)
+				}}
+				style={{ position: "absolute", left: "80%" }}
+			>
+				sign out
+			</button>
         <div className={appStyles.navLeft}>
           <button className={appStyles.sideBarBtn}></button>
           <Logo to={'/app/today'} />
