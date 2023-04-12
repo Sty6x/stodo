2cde6d9c1fd4f00675a1e8ee96883ff27c41f0f6
francis <jerylfranz460@gmail.com>
2023-04-13 02:39:05 +0400

    ref: add profile placeholder

--- src/pages/app/App.js
+++ src/pages/app/App.js
@@ -6,27 +6,27 @@ import appStyles from "./app.module.scss";
 import { signOut } from "firebase/auth";
 import { FirebaseContext } from "../../App";
 
-
 export const App = () => {
-  const {auth} = useContext(FirebaseContext);
+  const { auth } = useContext(FirebaseContext);
   return (
     <>
       <Navbar>
-			<button
-				onClick={async (e) => {
-          signOut(auth)
-				}}
-				style={{ position: "absolute", left: "80%" }}
-			>
-			Sign out	
-			</button>
+        <button
+          onClick={async (e) => {
+            signOut(auth);
+          }}
+          style={{ position: "absolute", left: "80%" }}
+        >
+          Sign out
+        </button>
         <div className={appStyles.navLeft}>
           <button className={appStyles.sideBarBtn}></button>
-          <Logo to={'/app/today'} />
+          <Logo to={"/app/today"} />
         </div>
         <div className={appStyles.navRight}>
           {/* make user profile drop down on click  */}
-          <button className={appStyles.profile}></button>
+          <div className={appStyles.profile}>
+          </div>
         </div>
       </Navbar>
       <main className={appStyles.appPage}>
