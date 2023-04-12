94f4a1ff333ac786332fc8a0c3208ddb8525f23c
francis <jerylfranz460@gmail.com>
2023-04-12 20:09:57 +0400

    ref: contained app within the main App component to make use of main element for main contents

--- src/pages/app/App.js
+++ src/pages/app/App.js
@@ -1,10 +1,15 @@
 import React, { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
+import { Navbar } from "../../components/navbar/Navbar";
+import appStyles from "./app.module.scss";
 
 export const App = () => {
-	return (
-		<div>
-			<Outlet />
-		</div>
-	);
+  return (
+    <>
+      <Navbar></Navbar>
+      <main className={appStyles.appPage}>
+        <Outlet />
+      </main>
+    </>
+  );
 };
