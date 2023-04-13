77e0cd9c86fb2ef63d1a52e6585753f78715233e
francis <jerylfranz460@gmail.com>
2023-04-13 20:34:54 +0400

    ref: add links to each page

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,7 +1,28 @@
 import React from "react";
 import sidebarStyles from "./sidebar.module.scss";
 import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
 export const Sidebar = () => {
-  return <motion.div className={sidebarStyles.sideBar}>Sidebar</motion.div>;
+  return (
+    <motion.div className={sidebarStyles.sideBar}>
+      <div className={sidebarStyles.contentsContainer}>
+        <ul className={sidebarStyles.navigation}>
+          <li className={sidebarStyles.today}>
+            <Link to={"/app/today"}>Today</Link>
+          </li>
+          <li className={sidebarStyles.upcoming}>
+            <Link to={"/app/upcoming"}>Upcoming</Link>
+          </li>
+          <li className={sidebarStyles.overdue}>
+            <Link to={"/app/overdue"}>Overdue Tasks</Link>
+          </li>
+        </ul>
+        <div className={sidebarStyles.projectContainer}>
+          <p>Projects</p>
+          <ul className={sidebarStyles.projectList}></ul>
+        </div>
+      </div>
+    </motion.div>
+  );
 };
