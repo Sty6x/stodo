9bc43577e9a972cdda6f5b7005717ec5c7d401a6
francis <jerylfranz460@gmail.com>
2023-04-17 01:40:24 +0400

    fix: nice

--- src/pages/auth-page/Auth.js
+++ src/pages/auth-page/Auth.js
@@ -1,15 +1,18 @@
+import { AnimatePresence, motion } from "framer-motion";
 import React from "react";
 import { Outlet } from "react-router-dom";
-import authPageStyles from './authpages.module.scss'
+import authPageStyles from "./authpages.module.scss";
 export const Auth = () => {
-  // handle the logic here for sign in and sign up 
-  // instead of changing a whole page for sign in and sign up 
+  // handle the logic here for sign in and sign up
+  // instead of changing a whole page for sign in and sign up
   // just the form component of sign in and sign up and
   // display the auth while form components sit on top of auth page
 
-	return (
-		<main className={authPageStyles.page}>
-			<Outlet />
-		</main>
-	);
+  return (
+    <motion.main className={authPageStyles.page}>
+      <AnimatePresence>
+        <Outlet />
+      </AnimatePresence>
+    </motion.main>
+  );
 };
