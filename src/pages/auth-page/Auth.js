7bb35d312727a6e9ae6dbdfa5cd4e63a86a2da85
francis <jerylfranz460@gmail.com>
2023-04-17 00:45:16 +0400

    refactoring auth page components

--- src/pages/auth-page/Auth.js
+++ src/pages/auth-page/Auth.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 export const Auth = () => {
+  // handle the logic here for sign in and sign up 
+  // instead of changing a whole page for sign in and sign up 
+  // just the form component of sign in and sign up and
+  // display the auth while form components sit on top of auth page
+
 	return (
 		<>
 			<Outlet />
