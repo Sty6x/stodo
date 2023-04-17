1da7bca553aabd8b29cb6f693581a9bb59d73bd1
francis <jerylfranz460@gmail.com>
2023-04-17 19:31:21 +0400

    ref: added content container and isolated style

--- src/pages/app/today/Today.js
+++ src/pages/app/today/Today.js
@@ -1,10 +1,18 @@
 import React, { useContext } from "react";
 import { FirebaseContext } from "../../../App";
-import todayStyles from './today.module.scss'
-import appPages from '../app.module.scss'
+import todayStyles from "./today.module.scss";
+import appPages from "../app.module.scss";
 export const Today = () => {
-  const {auth} = useContext(FirebaseContext);
-	return <div id="today-page" key='today-page' className={`${appPages.pages}`}>
-       <h1>Hello,{auth.currentUser.displayName}</h1> 
-    </div>;
+  const { auth } = useContext(FirebaseContext);
+  return (
+    <div
+      id="today-page"
+      key="today-page"
+      className={`${appPages.pages} ${todayStyles.todayPage}`}
+    >
+      <div className={`${todayStyles.contentContainer}`}>
+        <h1>Hello,{auth.currentUser.displayName}</h1>
+      </div>
+    </div>
+  );
 };
