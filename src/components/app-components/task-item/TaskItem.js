20f8006d6f42b4fe0f843ebaf8fd5f039375f037
francis <jerylfranz460@gmail.com>
2023-04-18 02:48:34 +0400

    ref: add folder and files for task items

---
+++ src/components/app-components/task-item/TaskItem.js
@@ -0,0 +1,8 @@
+import React from "react";
+import taskItemStyles from './taskItem.module.scss'
+
+export const TaskItem =()=>{
+  return <div className={`${taskItemStyles.task}`}>
+    Task
+  </div>
+}
