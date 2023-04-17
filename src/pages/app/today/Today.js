5169dd53cb7c26c39a245c9d730d19896e7fc20d
francis <jerylfranz460@gmail.com>
2023-04-18 02:45:24 +0400

    ref: structured folders

--- src/pages/app/today/Today.js
+++ src/pages/app/today/Today.js
@@ -3,8 +3,8 @@ import { FirebaseContext } from "../../../App";
 import todayStyles from "./today.module.scss";
 import appPages from "../app.module.scss";
 import { HeaderComponent } from "../../../components/app-components/header/HeaderComponent";
-import { PageLayout } from "../../../components/app-components/content/PageLayout";
 import { useOutletContext } from "react-router-dom";
+import { PageLayout } from "../../../components/app-components/page-layout/PageLayout";
 export const Today = () => {
   const { auth } = useContext(FirebaseContext);
   return (
