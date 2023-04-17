a3068da8a77cda661c683072dd107eade32b7125
francis <jerylfranz460@gmail.com>
2023-04-17 22:32:28 +0400

    ref: made header pages reusable components throughout different pages

--- src/pages/app/Upcoming.js
+++ src/pages/app/Upcoming.js
@@ -1,7 +1,9 @@
 import React from "react";
 import appPages from "./app.module.scss";
 export const Upcoming = () => {
-  return <div className={`${appPages.pages}`}>
-    <h1>Upcoming Tasks here</h1>;
-  </div>;
+  return (
+    <div className={`${appPages.pages}`}>
+      <h1>Upcoming Tasks here</h1>;
+    </div>
+  );
 };
--- src/pages/app/today/Today.js
+++ src/pages/app/today/Today.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { FirebaseContext } from "../../../App";
 import todayStyles from "./today.module.scss";
 import appPages from "../app.module.scss";
+import { HeaderComponent } from "../../../components/app-components/HeaderComponent";
 export const Today = () => {
   const { auth } = useContext(FirebaseContext);
   return (
@@ -10,13 +11,7 @@ export const Today = () => {
       key="today-page"
       className={`${appPages.pages} ${todayStyles.todayPage}`}
     >
-      <div className={`${todayStyles.contentContainer}`}>
-        {/* this should be a component */}
-        <div className={`${appPages.headerContainer}`}>
-             <h1>Today</h1> 
-              <p>Thu 23 Mar</p>
-        </div>
-      </div>
+      <HeaderComponent pageName={'Today'}/>
     </div>
   );
 };
