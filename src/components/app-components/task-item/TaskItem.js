df8b1c3abe13698d00c25cbfe05e0b6c4e95ac09
francis <jerylfranz460@gmail.com>
2023-04-18 20:08:05 +0400

    ref: add elements to compose task item

--- src/components/app-components/task-item/TaskItem.js
+++ src/components/app-components/task-item/TaskItem.js
@@ -1,9 +1,20 @@
 import React from "react";
-import taskItemStyles from './taskItem.module.scss'
+import taskItemStyles from "./taskItem.module.scss";
 
-export const TaskItem =({task})=>{
-  return <div className={`${taskItemStyles.task}`}>
-    <h3>{task.title}</h3>
-    <p>{task.description}</p>
-  </div>
-}
+export const TaskItem = ({ task }) => {
+  return (
+    <div className={`${taskItemStyles.task}`}>
+      <button className={`${taskItemStyles.doneBtn}`}/>
+      <div className={`${taskItemStyles.priorityTitleDescContainer}`}>
+        <div className={`${taskItemStyles.currentPriority}`}>
+          <span className={`${taskItemStyles.low}`}></span>
+          <span className={`${taskItemStyles.medium}`}></span>
+          <span className={`${taskItemStyles.high}`}></span>
+        </div>
+      <h3>{task.title}<span>2:32pm</span></h3>
+      <p>{task.description}</p>
+      </div>
+      <button className={`${taskItemStyles.taskOptions}`}/>
+    </div>
+  );
+};
