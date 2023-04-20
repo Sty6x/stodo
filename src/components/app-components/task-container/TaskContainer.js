a3b43164b7724ea0ebba38894a8411cdf4a7a33f
francis <jerylfranz460@gmail.com>
2023-04-20 23:34:34 +0400

    style: add options button and readjusted padding

--- src/components/app-components/task-container/TaskContainer.js
+++ src/components/app-components/task-container/TaskContainer.js
@@ -1,10 +1,6 @@
 import React from "react";
-import taskContainerStyles from './taskContainer.module.scss'
+import taskContainerStyles from "./taskContainer.module.scss";
 
-export const TaskContainer =({children})=>{
-  return <ul className={taskContainerStyles.container}>
-    {children}
-  </ul>
-}
-
-
+export const TaskContainer = ({ children }) => {
+  return <ul className={taskContainerStyles.container}>{children}</ul>;
+};
