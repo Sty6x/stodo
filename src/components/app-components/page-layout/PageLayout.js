cfa6097912c1cd8cdf8979ea94d34d9f0f9e8d68
francis <jerylfranz460@gmail.com>
2023-04-21 04:53:09 +0400

    ref: button will reside within page layout and on each page will determine the button's behaviour and text

--- src/components/app-components/page-layout/PageLayout.js
+++ src/components/app-components/page-layout/PageLayout.js
@@ -1,9 +1,10 @@
 import React from "react";
 import pageLayoutStyles from "./pageLayout.module.scss";
-export const PageLayout = ({ children }) => {
+export const PageLayout = ({ children, buttonType,handleBtnClick }) => {
   return (
     <div id="page-content-container" className={`${pageLayoutStyles.contentContainer}`}>
       {children}
+      <AddButton type={buttonType} handleClick={handleBtnClick}/>
     </div>
   );
 };
