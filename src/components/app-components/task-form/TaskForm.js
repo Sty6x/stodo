177e13eb0c1bddf4a5c44e27e1a17eb50496c29c
francis <jerylfranz460@gmail.com>
2023-04-25 01:44:37 +0400

    ...

--- src/components/app-components/task-form/TaskForm.js
+++ src/components/app-components/task-form/TaskForm.js
@@ -1,21 +1,25 @@
 import React from "react";
 import taskFormStyles from "./taskForm.module.scss";
 import { motion } from "framer-motion";
-import { Calendar} from 'primereact/calendar';
-import {SelectButton} from 'primereact/selectbutton'
-        
+import { Calendar } from "primereact/calendar";
+
+import { SelectButton } from "primereact/selectbutton";
+
 //theme
-import '../../../assets/themes/mytheme/theme.scss' 
+import "../../../assets/themes/mytheme/theme.scss";
 //core
-import "primereact/resources/primereact.min.css";                                       
+import "primereact/resources/primereact.min.css";
 
 export const TaskForm = ({ formRef, onSubmitHandler, cancelBtn }) => {
+  const options = ["Low", "Medium", "High"];
+
   return (
     <motion.div
-      initial={{opacity:0,y:100}}
-      animate={{opacity:1,y:0}}
-      exit={{opacity:0,y:50,transition:{duration:.1}}}
-      className={taskFormStyles.taskFormContainer}>
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: 50, transition: { duration: 0.1 } }}
+      className={taskFormStyles.taskFormContainer}
+    >
       <form
         onSubmit={onSubmitHandler}
         ref={formRef}
@@ -31,19 +35,18 @@ export const TaskForm = ({ formRef, onSubmitHandler, cancelBtn }) => {
           className={`${taskFormStyles.inputContainer} ${taskFormStyles.selectPriority}`}
         >
           <label htmlFor="priority">Select Task Priority:</label>
-          <SelectButton options={['low','medium','high']}/>
-          {/* <select name="taskPriority"> */}
-          {/*   <option value={"#008CFF"}>Low</option> */}
-          {/*   <option value={"#F4C70A"}>Medium</option> */}
-          {/*   <option value={"#FF2855"}>High</option> */}
-          {/* </select> */}
+          <select name="taskPriority">
+            <option value={"#008CFF"}>Low</option>
+            <option value={"#F4C70A"}>Medium</option>
+            <option value={"#FF2855"}>High</option>
+          </select>
         </div>
         <div
           className={`${taskFormStyles.inputContainer} ${taskFormStyles.dateInput}`}
         >
           {/* <label htmlFor="date">Date</label> */}
           {/* <input name="date" type="datetime-local" /> */}
-          <Calendar name="date" value={new Date()} showIcon/>
+          <Calendar name="date" value={new Date()} showIcon />
         </div>
         <div className={taskFormStyles.addAndCancelBtn}>
           <button type="submit">Add task</button>
