73d85b5b7ef12a45c576f6dbf9a6a2fb69a8f647
francis <jerylfranz460@gmail.com>
2023-04-27 11:41:08 +0400

    ref: conditioanl statement if using main header

--- src/components/app-components/header/HeaderComponent.js
+++ src/components/app-components/header/HeaderComponent.js
@@ -1,15 +1,15 @@
 import React from "react";
-import headerStyles from './headerComponent.module.scss'
+import headerStyles from "./headerComponent.module.scss";
+import { format } from "date-fns";
 
-export const HeaderComponent =({pageName}) =>{
-  return (
-      <header className={`${headerStyles.headerContainer}`}>
-        {/* this should be a component */}
-        <div className={`${headerStyles.titleDateContainer}`}>
-          <h1>{pageName}</h1>
-          <p>Thu 23 Mar</p>
-        </div>
-      </header>
-  )
-}
-
+export const HeaderComponent = ({ pageName, isMainHeader = false }) => {
+	return (
+		<header className={`${headerStyles.headerContainer}`}>
+			{/* this should be a component */}
+			<div className={`${headerStyles.titleDateContainer}`}>
+				<h1>{pageName}</h1>
+				{isMainHeader && <p>{format(new Date(), "PP")}</p>}
+			</div>
+		</header>
+	);
+};
--- src/pages/app/Upcoming.js
+++ src/pages/app/Upcoming.js
@@ -1,37 +1,41 @@
-import React, {useState,useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { HeaderComponent } from "../../components/app-components/header/HeaderComponent";
 import { PageLayout } from "../../components/app-components/page-layout/PageLayout";
 import { TaskDatabaseContext } from "./App";
 import { TaskItem } from "../../components/app-components/task-item/TaskItem";
 import appPages from "./app.module.scss";
-import { isFuture } from "date-fns";
+import { format, isFuture } from "date-fns";
+import { TaskContainer } from "../../components/app-components/task-container/TaskContainer";
 export const Upcoming = () => {
-  const {tasks} = useContext(TaskDatabaseContext)
-  const [upcomingTasks, setUpcomingTasks] = useState([]);
+	const { tasks } = useContext(TaskDatabaseContext);
+	const [upcomingTasks, setUpcomingTasks] = useState([]);
 
-  useEffect(() => {
-    filterTasks(tasks);
-  }, [tasks]);
+	useEffect(() => {
+		filterTasks(tasks);
+	}, [tasks]);
 
-  async function filterTasks(tasks) {
-    const filteredTasks = tasks.filter((task) => {
-      if (isFuture( new Date(task.dueDate))) {
-        return task;
-      }
-    });
-    console.log(filteredTasks);
-    setUpcomingTasks(filteredTasks);
-  }
+	async function filterTasks(tasks) {
+		const filteredTasks = tasks.filter((task) => {
+			if (isFuture(new Date(task.dueDate))) {
+				return task;
+			}
+		});
+		console.log(filteredTasks);
+		setUpcomingTasks(filteredTasks);
+	}
 
-  const appendTasks = upcomingTasks.map((task) => {
-    return <TaskItem /* deleteTask={deleteTask}  */key={task.ID} task={task} />;
-  });
-  return (
-    <div className={`${appPages.pages}`}>
-      <HeaderComponent pageName={'Upcoming'}/>
-      <PageLayout>
-       {appendTasks} 
-      </PageLayout>
-    </div>
-  );
+	const appendTasks = upcomingTasks.map((task) => {
+		return (
+			<TaskContainer>
+				<HeaderComponent pageName={format(new Date(task.dueDate), "PP")} />
+				<TaskItem /* deleteTask={deleteTask}  */ key={task.ID} task={task} />
+			</TaskContainer>
+		);
+	});
+	return (
+		<div className={`${appPages.pages}`}>
+			<HeaderComponent pageName={"Upcoming"} isMainHeader={true} />
+			<PageLayout>{appendTasks}</PageLayout>
+		</div>
+	);
 };
