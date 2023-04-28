01772ed00b708d00c5bb07a9f83b6c0db42d9248
francis <jerylfranz460@gmail.com>
2023-04-28 14:35:14 +0400

    ref: funtion to filter and sort tasks by due date

--- src/pages/app/Overdue.js
+++ src/pages/app/Overdue.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import appPages from "./app.module.scss";
 import { HeaderComponent } from "../../components/app-components/header/HeaderComponent";
 import { PageLayout } from "../../components/app-components/page-layout/PageLayout";
+import { TaskDatabaseContext } from "./App";
+import { isFuture, isPast } from "date-fns";
 
 export const Overdue = () => {
+  const { tasks } = useContext(TaskDatabaseContext);
+
+  useEffect(() => {
+    filterAndSortTasksByDueDate(tasks);
+  }, [tasks]);
+
+  async function filterAndSortTasksByDueDate(tasks) {
+    const filteredTasks = tasks.filter((task) => {
+      return task && isPast(new Date(task.dueDate));
+    });
+    console.log(filteredTasks);
+  }
+
   return (
     <div className={`${appPages.pages}`}>
       <HeaderComponent pageName={"Overdue"} isMainHeader={true} />
