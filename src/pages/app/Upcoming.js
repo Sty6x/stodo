315a74b85f97c24b3e2006d0cf7752f4d6c003e1
francis <jerylfranz460@gmail.com>
2023-04-28 14:26:58 +0400

    removed unneccesary code

--- src/pages/app/Upcoming.js
+++ src/pages/app/Upcoming.js
@@ -11,9 +11,7 @@ export const Upcoming = () => {
   const [upcomingTasks, setUpcomingTasks] = useState([]);
 
   useEffect(() => {
-    filterTasks(tasks).then((t) => {
-      containFilteredTask(t);
-    });
+    filterTasks(tasks);
   }, [tasks]);
 
   async function filterTasks(tasks) {
@@ -22,26 +20,8 @@ export const Upcoming = () => {
         return task;
       }
     });
-    // just sort them
     setUpcomingTasks(filteredTasks);
     return filteredTasks;
-  }
-
-  function containFilteredTask(t) {
-    for (let i = 0; i < t.length; i++) {
-      let tmpArr = [];
-      for (let j = 0; j < t.length; j++) {
-        if (compareAsc(new Date(t[i].dueDate), new Date(t[j].dueDate)) === 0) {
-          tmpArr.push(t[j]);
-        }
-      }
-
-      if (tmpArr.length == 1) {
-        console.log(tmpArr);
-      } else if (tmpArr.length > 1) {
-        console.log(tmpArr);
-      }
-    }
   }
 
   const appendTasks = upcomingTasks.map((task, i) => {
