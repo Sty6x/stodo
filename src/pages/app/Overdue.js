06c86f0355cc671bdaf064593b40558c06ee9df3
francis <jerylfranz460@gmail.com>
2023-04-28 14:45:59 +0400

    ref: changed sorted direction to descending order and appended overdue tasks

--- src/pages/app/Overdue.js
+++ src/pages/app/Overdue.js
@@ -1,15 +1,19 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import appPages from "./app.module.scss";
 import { HeaderComponent } from "../../components/app-components/header/HeaderComponent";
 import { PageLayout } from "../../components/app-components/page-layout/PageLayout";
 import { TaskDatabaseContext } from "./App";
 import { isFuture, isPast } from "date-fns";
+import { TaskItem } from "../../components/app-components/task-item/TaskItem";
 
 export const Overdue = () => {
   const { tasks } = useContext(TaskDatabaseContext);
+  const [overdueTasks, setOverdueTasks] = useState([]);
 
   useEffect(() => {
-    filterAndSortTasksByDueDate(tasks);
+    filterAndSortTasksByDueDate(tasks).then((data) => {
+      setOverdueTasks(data);
+    });
   }, [tasks]);
 
   async function filterAndSortTasksByDueDate(tasks) {
@@ -17,15 +21,20 @@ export const Overdue = () => {
       return task && isPast(new Date(task.dueDate));
     });
     const sortFilteredTasks = filteredTasks.sort(
-      (a, b) => new Date(a.dueDate) - new Date(b.dueDate)
+      (a, b) => new Date(b.dueDate) - new Date(a.dueDate)
     );
     console.log(sortFilteredTasks);
+    return sortFilteredTasks;
   }
+
+  const appendOverdueTasks = overdueTasks.map((task) => {
+    return <TaskItem key={task.ID} task={task} />;
+  });
 
   return (
     <div className={`${appPages.pages}`}>
       <HeaderComponent pageName={"Overdue"} isMainHeader={true} />
-      <PageLayout></PageLayout>
+      <PageLayout>{appendOverdueTasks}</PageLayout>
     </div>
   );
 };
