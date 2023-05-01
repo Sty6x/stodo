170bf79b9a92904d38d84aea7d7a59be1e0d506a
francis <jerylfranz460@gmail.com>
2023-05-01 21:47:59 +0400

    style: add hover and active effect to give uiser feedback

--- src/components/app-components/task-item/TaskItem.js
+++ src/components/app-components/task-item/TaskItem.js
@@ -9,24 +9,29 @@ export const TaskItem = ({
   deleteTask,
 }) => {
   return (
-    <motion.li
-      layout
-      id={ID}
-      className={`${taskItemStyles.task}`}
-    >
+    <motion.li layout id={ID} className={`${taskItemStyles.task}`}>
       <button
         onClick={(e) => deleteTask(ID)}
         className={`${taskItemStyles.doneBtn}`}
       />
       <div className={`${taskItemStyles.taskContentContainer}`}>
         <div className={`${taskItemStyles.currentPriority}`}>
-          <span style={{backgroundColor: taskPriority === 'Low' &&  '#008CFF'}} className={`${taskItemStyles.low}`}></span>
-          <span style={{backgroundColor: taskPriority === 'Medium' &&  '#F4C70A'}} className={`${taskItemStyles.medium}`}></span>
-          <span style={{backgroundColor: taskPriority === 'High' &&  '#FF2855'}} className={`${taskItemStyles.high}`}></span>
+          <span
+            style={{ backgroundColor: taskPriority === "Low" && "#008CFF" }}
+            className={`${taskItemStyles.low}`}
+          ></span>
+          <span
+            style={{ backgroundColor: taskPriority === "Medium" && "#F4C70A" }}
+            className={`${taskItemStyles.medium}`}
+          ></span>
+          <span
+            style={{ backgroundColor: taskPriority === "High" && "#FF2855" }}
+            className={`${taskItemStyles.high}`}
+          ></span>
         </div>
         <h3>
           {title}
-          <span>{format(new Date(dateAdded),'p')}</span>
+          <span>{format(new Date(dateAdded), "p")}</span>
         </h3>
         <p>{desc}</p>
       </div>
