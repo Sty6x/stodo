66988e8967db3be823d18cdebe2bfa6aab8e13a9
francis <jerylfranz460@gmail.com>
2023-05-02 12:46:16 +0400

    ref: built input project links

--- src/components/project-link-input/ProjectLinkInput.js
+++ src/components/project-link-input/ProjectLinkInput.js
@@ -1,1 +1,11 @@
- 
+import React from "react";
+import projectLinkStyle from './projectLinkInput.module.scss'
+export const ProjectLinkInput = () => {
+  return (
+    <div className={projectLinkStyle.container}>
+      <label htmlFor="projectName">Project Name</label>
+      <input className={projectLinkStyle.name} type={"text"} id="prjectName" name="projectName" />
+      <button className={projectLinkStyle.btn} />
+    </div>
+  );
+};
