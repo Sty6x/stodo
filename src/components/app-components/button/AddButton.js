391fb961a59c1b0bbbec6dc053ea852a63598241
francis <jerylfranz460@gmail.com>
2023-05-04 13:02:26 +0400

    ref: made Addbutton separate and reusable

--- src/components/app-components/button/AddButton.js
+++ src/components/app-components/button/AddButton.js
@@ -1,10 +1,34 @@
 import React from "react";
 import addBtnStyles from "./addButton.module.scss";
+import { formControl } from "../../../utils/useFormControl";
+import { useState } from "react";
+import { AnimatePresence,motion } from "framer-motion";
+import { TaskForm } from "../task-form/TaskForm";
 
-export const AddButton = ({ handleClick, type }) => {
+export const AddButton = ({addTask, handleClick, type }) => {
+  const [formActive, setFormActive] = useState(false);
   return (
-    <button className={addBtnStyles.button} onClick={handleClick}>
-      {type}
-    </button>
+    <AnimatePresence mode="wait">
+      {formActive ? (
+        <TaskForm
+          key={"taskForm"}
+          formActive={formActive}
+          setFormActive={setFormActive}
+          onSubmitHandler={addTask}
+        />
+      ) : (
+        <motion.button
+          layout
+          exit={{ y: -20, opacity: 0, transition: { duration: 0.1 } }}
+          animate={{ y: [-20, 0], opacity: [0, 1] }}
+          className={`${addBtnStyles.button}`}
+          onClick={() => {
+            formControl(setFormActive, formActive);
+          }}
+        >
+          Add Task
+        </motion.button>
+      )}
+    </AnimatePresence>
   );
 };
