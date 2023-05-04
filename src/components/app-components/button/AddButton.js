e3650a8bc1a8588745e1ca10795094714430360e
francis <jerylfranz460@gmail.com>
2023-05-04 13:17:05 +0400

    ref: dont need form control util when formcontrol can be handled within the component AddButton

--- src/components/app-components/button/AddButton.js
+++ src/components/app-components/button/AddButton.js
@@ -2,18 +2,20 @@ import React from "react";
 import addBtnStyles from "./addButton.module.scss";
 import { formControl } from "../../../utils/useFormControl";
 import { useState } from "react";
-import { AnimatePresence,motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { TaskForm } from "../task-form/TaskForm";
 
-export const AddButton = ({addTask, handleClick, type }) => {
+export const AddButton = ({ addTask, type }) => {
   const [formActive, setFormActive] = useState(false);
+  function formControl() {
+    return formActive ? setFormActive(false) : setFormActive(true);
+  }
   return (
     <AnimatePresence mode="wait">
       {formActive ? (
         <TaskForm
           key={"taskForm"}
-          formActive={formActive}
-          setFormActive={setFormActive}
+          formControl={formControl}
           onSubmitHandler={addTask}
         />
       ) : (
@@ -22,11 +24,9 @@ export const AddButton = ({addTask, handleClick, type }) => {
           exit={{ y: -20, opacity: 0, transition: { duration: 0.1 } }}
           animate={{ y: [-20, 0], opacity: [0, 1] }}
           className={`${addBtnStyles.button}`}
-          onClick={() => {
-            formControl(setFormActive, formActive);
-          }}
+          onClick={formControl}
         >
-          Add Task
+          {type}
         </motion.button>
       )}
     </AnimatePresence>
