254c9368bdf87d49cb234a1ac5cc1029e4c0d1da
francis-lp <jerylfranz460@gmail.com>
2023-05-08 11:22:49 +0100

    fixed: by updating currentmousepos with the usermousepos we dont have to meet the previous requirement of having the mouse scroll past the currentmousepos to either scroll left or right and instead update the currentmousepos with the user's mouse position

--- src/components/app-components/project-components/project-page-layout/ProjectPageLayout.js
+++ src/components/app-components/project-components/project-page-layout/ProjectPageLayout.js
@@ -1,26 +1,31 @@
 import React from "react";
 import projectPageLayoutStyle from "./projectPageLayout.module.scss";
+import { toHaveErrorMessage } from "@testing-library/jest-dom/dist/matchers";
 
 export const ProjectPageLayout = ({ children }) => {
   let currentMousePos = 0
   let isMouseDown = false;
   function mouseMoveHandler (e){
     const page = e.target
-    let nativeEventMouse = e.nativeEvent.offsetX;
-    console.log({x:nativeEventMouse})
+    let userMousePos = e.nativeEvent.offsetX  ;
+    console.log({x:userMousePos})
     if(isMouseDown){
-
-    if( nativeEventMouse >= currentMousePos){
-         page.scrollTo(nativeEventMouse++,0)
+      // if a user is pulling the page to scroll left or right and so that the users mouse dont need to scoll all the way to the end of the page
+      // page.scrollTo(userMousePos ,0)
+    if( userMousePos > currentMousePos ){
+         page.scrollTo(page.scrollLeft-=8,0)
     }
-    if(nativeEventMouse <= currentMousePos){
-       page.scrollTo(nativeEventMouse--,0)
+    if(userMousePos < currentMousePos ){
+       page.scrollTo(page.scrollLeft+=8,0)
     }
     }
+    // updates the currentmousepos after scrolling
+    currentMousePos = userMousePos
   }
   function mouseDownHandler(e){
     isMouseDown = true
     currentMousePos = e.nativeEvent.offsetX
+    console.log(currentMousePos)
   }
   function mouseUpHandler(e){
     isMouseDown = false
