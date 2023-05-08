2bf102829fce7827c45b03b7bd6575fc557e0264
francis <jerylfranz460@gmail.com>
2023-05-08 11:51:53 +0400

    square one

--- src/components/app-components/project-components/project-page-layout/ProjectPageLayout.js
+++ src/components/app-components/project-components/project-page-layout/ProjectPageLayout.js
@@ -2,17 +2,13 @@ import React from "react";
 import projectPageLayoutStyle from "./projectPageLayout.module.scss";
 
 export const ProjectPageLayout = ({ children }) => {
-  function mouseMoveHandler(e) {
-    const target = e.nativeEvent;
-    const mouseX = target.offsetX;
-    const mouseY = target.offsetY;
-    console.log({ x: mouseX, y: mouseY });
-    window.scrollTo(mouseX - e.target.innerWidth, 0);
-  }
+  let mousePos = { x: 0, y: 0 };
+
+
 
   return (
     <section
-      onMouseMove={mouseMoveHandler}
+  
       className={projectPageLayoutStyle.pageLayout}
     >
       {children}
