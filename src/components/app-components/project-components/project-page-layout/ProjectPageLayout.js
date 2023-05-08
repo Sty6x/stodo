6fef979f642e06112b6e4b9b01438aa36f2ddd73
francis <jerylfranz460@gmail.com>
2023-05-08 15:02:12 +0400

    style: set cursor grab

--- src/components/app-components/project-components/project-page-layout/ProjectPageLayout.js
+++ src/components/app-components/project-components/project-page-layout/ProjectPageLayout.js
@@ -1,42 +1,46 @@
-import React from "react";
+import React, { useRef } from "react";
 import projectPageLayoutStyle from "./projectPageLayout.module.scss";
 import { toHaveErrorMessage } from "@testing-library/jest-dom/dist/matchers";
 
 export const ProjectPageLayout = ({ children }) => {
-  let currentMousePos = 0
+  let currentMousePos = 0;
   let isMouseDown = false;
-  function mouseMoveHandler (e){
-    const page = e.target
-    let userMousePos = e.nativeEvent.offsetX  ;
-    console.log({x:userMousePos})
-    if(isMouseDown){
+
+  function mouseMoveHandler(e) {
+    const page = e.target;
+    let userMousePos = e.nativeEvent.offsetX;
+    console.log({ x: userMousePos });
+    if (isMouseDown) {
       // if a user is pulling the page to scroll left or right and so that the users mouse dont need to scoll all the way to the end of the page
       // page.scrollTo(userMousePos ,0)
-    if( userMousePos > currentMousePos ){
-         page.scrollTo(page.scrollLeft-=8,0)
-    }
-    if(userMousePos < currentMousePos ){
-       page.scrollTo(page.scrollLeft+=8,0)
-    }
+      if (userMousePos > currentMousePos) {
+        page.scrollTo((page.scrollLeft -= 3), 0);
+      }
+      if (userMousePos < currentMousePos) {
+        page.scrollTo((page.scrollLeft += 3), 0);
+      }
     }
     // updates the currentmousepos after scrolling
-    currentMousePos = userMousePos
+    currentMousePos = userMousePos;
   }
-  function mouseDownHandler(e){
-    isMouseDown = true
-    currentMousePos = e.nativeEvent.offsetX
-    console.log(currentMousePos)
+  function mouseDownHandler(e) {
+    const target = e.target;
+    target.style.cursor = "grabbing";
+    isMouseDown = true;
+    currentMousePos = e.nativeEvent.offsetX;
+    console.log(currentMousePos);
   }
-  function mouseUpHandler(e){
-    isMouseDown = false
+  function mouseUpHandler(e) {
+    const target = e.target;
+    isMouseDown = false;
+    target.style.cursor = "grab";
   }
-
 
   return (
     <section
-    onMouseMove={mouseMoveHandler}
-    onMouseDown={mouseDownHandler}
-    onMouseUp={mouseUpHandler}
+      onMouseMove={mouseMoveHandler}
+      onMouseDown={mouseDownHandler}
+      onMouseUp={mouseUpHandler}
       className={projectPageLayoutStyle.pageLayout}
     >
       {children}
