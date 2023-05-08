3c76abd2fb7ce55bc4444c9c1f981ee363980fcb
francis-lp <jerylfranz460@gmail.com>
2023-05-08 10:36:22 +0100

    ref: able to scroll mouse horizontally on mousedown

--- src/components/app-components/project-components/project-page-layout/ProjectPageLayout.js
+++ src/components/app-components/project-components/project-page-layout/ProjectPageLayout.js
@@ -2,13 +2,36 @@ import React from "react";
 import projectPageLayoutStyle from "./projectPageLayout.module.scss";
 
 export const ProjectPageLayout = ({ children }) => {
-  let mousePos = { x: 0, y: 0 };
+  let currentMousePos = 0
+  let isMouseDown = false;
+  function mouseMoveHandler (e){
+    const page = e.target
+    let nativeEventMouse = e.nativeEvent.offsetX;
+    console.log({x:nativeEventMouse})
+    if(isMouseDown){
 
+    if( nativeEventMouse >= currentMousePos){
+         page.scrollTo(nativeEventMouse++,0)
+    }
+    if(nativeEventMouse <= currentMousePos){
+       page.scrollTo(nativeEventMouse--,0)
+    }
+    }
+  }
+  function mouseDownHandler(e){
+    isMouseDown = true
+    currentMousePos = e.nativeEvent.offsetX
+  }
+  function mouseUpHandler(e){
+    isMouseDown = false
+  }
 
 
   return (
     <section
-  
+    onMouseMove={mouseMoveHandler}
+    onMouseDown={mouseDownHandler}
+    onMouseUp={mouseUpHandler}
       className={projectPageLayoutStyle.pageLayout}
     >
       {children}
