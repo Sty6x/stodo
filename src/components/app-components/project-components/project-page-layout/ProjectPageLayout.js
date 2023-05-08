d98a78206a3b50103c3e6260a9f58cfab80406d9
francis <jerylfranz460@gmail.com>
2023-05-08 11:15:20 +0400

    ref: replaced to offset

--- src/components/app-components/project-components/project-page-layout/ProjectPageLayout.js
+++ src/components/app-components/project-components/project-page-layout/ProjectPageLayout.js
@@ -2,7 +2,20 @@ import React from "react";
 import projectPageLayoutStyle from "./projectPageLayout.module.scss";
 
 export const ProjectPageLayout = ({ children }) => {
+  function mouseMoveHandler(e) {
+    const target = e.nativeEvent;
+    const mouseX = target.offsetX;
+    const mouseY = target.offsetY;
+    console.log({ x: mouseX, y: mouseY });
+    window.scrollTo(mouseX - e.target.innerWidth, 0);
+  }
+
   return (
-    <section className={projectPageLayoutStyle.pageLayout}>{children}</section>
+    <section
+      onMouseMove={mouseMoveHandler}
+      className={projectPageLayoutStyle.pageLayout}
+    >
+      {children}
+    </section>
   );
 };
