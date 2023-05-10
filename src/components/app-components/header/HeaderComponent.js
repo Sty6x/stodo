716f1818ea0e79a10f36602d081fa5d80c877baa
francis <jerylfranz460@gmail.com>
2023-05-10 17:00:22 +0400

    ref: add date to header component

--- src/components/app-components/header/HeaderComponent.js
+++ src/components/app-components/header/HeaderComponent.js
@@ -7,7 +7,7 @@ export const HeaderComponent = ({ pageName, isMainHeader = false }) => {
     <header className={`${headerStyles.headerContainer}`}>
       <div className={`${headerStyles.titleDateContainer}`}>
         <h1>{pageName}</h1>
-        {/* {isMainHeader && <p>{format(new Date(), "PP")}</p>} */}
+        <h1 className={headerStyles.date}>{format(new Date(), "PP")}</h1>
       </div>
     </header>
   );
