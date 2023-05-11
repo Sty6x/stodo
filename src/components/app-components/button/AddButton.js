bc7ed569472f87bb0413f4442d03a13517c964f2
francis <jerylfranz460@gmail.com>
2023-05-12 03:29:03 +0400

    style: changed color to make it more uniformed

--- src/components/app-components/button/AddButton.js
+++ src/components/app-components/button/AddButton.js
@@ -19,7 +19,6 @@ export const AddButton = ({formActive, formControl, addTask, type = "default" })
       ) : (
         <motion.button
           style={{ color: type === "project" ? "#575757" : "#FFFFFF" }}
-          // layout
           exit={{ y: -20, opacity: 0, transition: { duration: 0.1 } }}
           animate={{ y: [-20, 0], opacity: [0, 1] }}
           className={`${addBtnStyles.button}`}
