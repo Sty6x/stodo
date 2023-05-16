b57c27db1a073bf410200e0d8f596cae12fa6303
francis <franzdiaz460@gmail.com>
2023-05-17 00:39:59 +0400

    style: adjusting phone sign in

--- src/components/sidebar/project-actions/ProjectActions.js
+++ src/components/sidebar/project-actions/ProjectActions.js
@@ -2,53 +2,55 @@ import React, { useRef, useState } from "react";
 import projectActionsStyle from "./projectActions.module.scss";
 
 export const ProjectActions = ({
-  handleCancelButton,
-  handleEditButton,
-  handleDeleteButton,
-  projectName,
-  projectID,
+	handleCancelButton,
+	handleEditButton,
+	handleDeleteButton,
+	projectName,
+	projectID,
 }) => {
-  const [inputInvalid, setInputInvalid] = useState(true);
-  function checkProjectNameInput(e) {
-    const target = e.target;
-    if (target.validity.valueMissing || target.value === "") {
-      return setInputInvalid(true);
-    }
-    return setInputInvalid(false);
-  }
+	const [inputInvalid, setInputInvalid] = useState(true);
+	function checkProjectNameInput(e) {
+		const target = e.target;
+		if (target.validity.valueMissing || target.value === "") {
+			return setInputInvalid(true);
+		}
+		return setInputInvalid(false);
+	}
 
-  return (
-          <div className={projectActionsStyle.container}>
-      <div className={projectActionsStyle.titleCloseBtn}>
-        <h1>Edit Project</h1>
-        <button onClick={handleCancelButton} />
-      </div>
-      <form
-        className={projectActionsStyle.input}
-        onSubmit={(e) => {
-          handleEditButton(e, projectID);
-        }}
-        noValidate
-      >
-        <label htmlFor="newProjectName">Change Name</label>
-        <input
-          onChange={checkProjectNameInput}
-          name="projectName"
-          id="newProjectName"
-          placeholder={projectName}
-        />
+	return (
+		<div className={projectActionsStyle.container}>
+			<div className={projectActionsStyle.titleCloseBtn}>
+				<h1>Edit Project</h1>
+				<button onClick={handleCancelButton} />
+			</div>
+			<form
+				className={projectActionsStyle.input}
+				onSubmit={(e) => {
+					handleEditButton(e, projectID);
+				}}
+				noValidate
+			>
+				<label htmlFor="newProjectName">Change Name</label>
+				<input
+					onChange={checkProjectNameInput}
+					name="projectName"
+					id="newProjectName"
+					placeholder={projectName}
+				/>
 
-        <div className={projectActionsStyle.buttons}>
-          <button disabled={inputInvalid} type="submit">Save changes</button>
-          <button
-            onClick={(e) => {
-              handleDeleteButton(e, projectID);
-            }}
-          >
-            Remove
-          </button>
-        </div>
-      </form>
-          </div>
-  );
+				<div className={projectActionsStyle.buttons}>
+					<button disabled={inputInvalid} type="submit">
+						Save changes
+					</button>
+					<button
+						onClick={(e) => {
+							handleDeleteButton(e, projectID);
+						}}
+					>
+						Remove
+					</button>
+				</div>
+			</form>
+		</div>
+	);
 };
--- src/components/sidebar/project-link/ProjectLink.js
+++ src/components/sidebar/project-link/ProjectLink.js
@@ -5,63 +5,67 @@ import { motion, AnimatePresence } from "framer-motion";
 import { ProjectActions } from "../project-actions/ProjectActions";
 
 export const ProjectLink = ({
-  to,
-  projectName,
-  projectData,
-  deleteProject,
-  editProject,
+	to,
+	projectName,
+	projectData,
+	deleteProject,
+	editProject,
 }) => {
-  const [optionsIsActive, setOptionsIsActive] = useState(false);
-  const [isHovering, setIsHovering] = useState(false);
-  function setOptions(e) {
-    e.preventDefault();
-    return optionsIsActive
-      ? setOptionsIsActive(false)
-      : setOptionsIsActive(true);
-  }
+	const [optionsIsActive, setOptionsIsActive] = useState(false);
+	const [isHovering, setIsHovering] = useState(false);
+	function setOptions(e) {
+		e.preventDefault();
+		return optionsIsActive
+			? setOptionsIsActive(false)
+			: setOptionsIsActive(true);
+	}
 
-  return (
-    <motion.li
-      onHoverStart={(e) => {
-        setIsHovering(true);
-        e.stopPropagation();
-      }}
-      onHoverEnd={(e) => {
-        setIsHovering(false);
-      }}
-      layout
-      animate={{ y: 0, opacity: 1 }}
-      className={`${projectLinkStyles.linkContainer}`}
-      key={projectName}
-    >
-      <AnimatePresence mode="wait">
-        {optionsIsActive ? (
-          <ProjectActions
-            handleDeleteButton={deleteProject}
-            handleCancelButton={setOptions}
-            handleEditButton={editProject}
-            projectID={projectData.ID}
-            projectName={projectData.projectName}
-          />
-        ) : (
-          <NavLink className={`${projectLinkStyles.link}`} key={to} to={to}>
-            {projectName}
-            {isHovering ? (
-              <motion.button
-                className={projectLinkStyles.actionBtn}
-                onClick={setOptions}
-              >
-                <span />
-              </motion.button>
-            ) : (
-              <p>
-                {projectData.sectionTasks.length !== 0
-                  && projectData.sectionTasks.length}
-              </p>
-            )}
-          </NavLink>
-        )}
-      </AnimatePresence>
-    </motion.li>
-  );
+	return (
+		<motion.li
+			onHoverStart={(e) => {
+				setIsHovering(true);
+				e.stopPropagation();
+			}}
+			onHoverEnd={(e) => {
+				setIsHovering(false);
+			}}
+			animate={{ y: 0, opacity: 1 }}
+			className={`${projectLinkStyles.linkContainer}`}
+			key={projectName}
+		>
+			<AnimatePresence mode="wait">
+				{optionsIsActive ? (
+					<ProjectActions
+						key={"projectActions"}
+						handleDeleteButton={deleteProject}
+						handleCancelButton={setOptions}
+						handleEditButton={editProject}
+						projectID={projectData.ID}
+						projectName={projectData.projectName}
+					/>
+				) : (
+					<NavLink
+						className={`${projectLinkStyles.link}`}
+						key={to}
+						to={to}
+					>
+						{projectName}
+						{isHovering ? (
+							<motion.button
+								className={projectLinkStyles.actionBtn}
+								onClick={setOptions}
+							>
+								<span />
+							</motion.button>
+						) : (
+							<p>
+								{projectData.sectionTasks.length !== 0 &&
+									projectData.sectionTasks.length}
+							</p>
+						)}
+					</NavLink>
+				)}
+			</AnimatePresence>
+		</motion.li>
+	);
 };
